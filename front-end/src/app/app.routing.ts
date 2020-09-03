b2363a5eacd1f90151011986989ea300cdf0690c
Carlos H. Araújo <carloshaam@gmail.com>
2020-09-03 18:57:22 -0300

    organização do projeto

---
+++ front-end/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { CongratulationsComponent } from './congratulations/congratulations.component';
+
+const APP_ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'confirmation', component: ConfirmationComponent },
+    { path: 'congratulations', component: CongratulationsComponent }
+];
+
+const config: ExtraOptions = {
+    useHash: true
+};
+
+@NgModule({
+    imports: [RouterModule.forRoot(APP_ROUTES, config)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
