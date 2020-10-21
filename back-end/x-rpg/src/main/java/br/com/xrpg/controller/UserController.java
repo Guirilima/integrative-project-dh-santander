86eaacba911212561d5c00dbfca8077e3babc7ef
Guirilima <Guirilima@gmail.com>
2020-10-20 22:10:37 -0300

    merge

--- back-end/x-rpg/src/main/java/br/com/xrpg/controller/UserController.java
+++ back-end/x-rpg/src/main/java/br/com/xrpg/controller/UserController.java
@@ -3,10 +3,7 @@ package br.com.xrpg.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.xrpg.entity.UserEntity;
 import br.com.xrpg.service.UserService;
