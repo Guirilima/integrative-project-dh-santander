1a92665b23c924413bd69269433008d898406934
Otavio Miranda <otavio.mpm@gmail.com>
2020-12-02 16:22:47 -0300

    ajustes finais

--- front-end/src/app/personagem/personagem.component.ts
+++ front-end/src/app/personagem/personagem.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Personagem } from '../shared/personagem.model';
 import { LoggedUserService } from '../logged-user.service';
@@ -17,6 +17,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 export class PersonagemComponent implements OnInit {
 
+
+  apiURL = 'http://localhost:8080/api';
+
+  @Input() idRacaPersonagem: any;
+
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -24,30 +29,74 @@ export class PersonagemComponent implements OnInit {
     })
   };
 
+  
+  NomeClasse;
+  NomeRaca;
+
   PERSONAGENS = null;
+  RACAS = null;
+  CLASSES = null;
+  
 
   @ViewChild('modalConvite') modalConvite: any;
 
   constructor(private router: Router, private http: HttpClient, private loggedUserService: LoggedUserService, private modalService: NgbModal, config: NgbModalConfig) {
 
     if (this.loggedUserService.isLogged()) {
-      var promiseClasses = this.http.get(`${URL_API}/api/personagem?pagina=0&qtdPagina=100`,
+      var promisePersonagem = this.http.get(`${URL_API}/api/personagem?pagina=0&qtdPagina=100`,
         this.httpOptions)
         .toPromise();
 
-      promiseClasses.then((data) => {
+      promisePersonagem.then((data) => {
 
         var jsonInfo = JSON.stringify(data)
         // console.log(jsonInfo);
         var Info = JSON.parse(jsonInfo);
         this.PERSONAGENS = Info.response.data;
-
+        console.log(this.PERSONAGENS);
 
 
       }).catch((error) => {
 
         console.log("Promise rejected with " + JSON.stringify(error));
       })
+////
+      var promiseNomeRaca = this.http.get(`${URL_API}/api/raca/listar-racas?pagina=0&qtdPagina=100`,
+        this.httpOptions)
+        .toPromise();
+
+        promiseNomeRaca.then((data) => {
+
+        var jsonInfo = JSON.stringify(data)
+        // console.log(jsonInfo);
+        var Info = JSON.parse(jsonInfo);
+        this.RACAS = Info.response.data;
+        console.log(this.RACAS);
+
+
+      }).catch((error) => {
+
+        console.log("Promise rejected with " + JSON.stringify(error));
+      })
+
+      var promiseNomeClasse = this.http.get(`${URL_API}/api/classe/listar-classes`,
+      this.httpOptions)
+      .toPromise();
+
+      promiseNomeClasse.then((response) => {
+
+      var jsonInfo = JSON.stringify(response)
+      // console.log(jsonInfo);
+      var Info = JSON.parse(jsonInfo);
+      this.CLASSES = Info.response;
+      console.log(this.CLASSES);
+
+
+    }).catch((error) => {
+
+      console.log("Promise rejected with " + JSON.stringify(error));
+    })
+////
     }
   }
   ngOnInit(): void {
@@ -57,13 +106,12 @@ export class PersonagemComponent implements OnInit {
       this.router.navigate(['/login']);
     }
 
-
   }
 
   salvarPersonagem(idConvidado, idPersonagem) {
     sessionStorage.setItem('idPersonagem', idPersonagem);
     sessionStorage.setItem('idConvidado', idConvidado);
-    sessionStorage.setItem('idMestre',null);
+    sessionStorage.setItem('idMestre', null);
     // this.router.navigate((['/convidar']));
   }
 
@@ -73,14 +121,16 @@ export class PersonagemComponent implements OnInit {
     this.modalService.open(this.modalConvite);
   }
 
-  proprioPersonagem(idUsuario)
-  {
-    if(idUsuario === this.loggedUserService.getId()) return false;
+  proprioPersonagem(idUsuario) {
+    if (idUsuario === this.loggedUserService.getId()) return false;
     return true;
   }
 
-//   print(){
-//    console.log("deu boa")
-//  }
+  //   print(){
+  //    console.log("deu boa")
+  //  }
 
+  idClasseToNomeClasse(idClasse){
+    
+  }
 }
